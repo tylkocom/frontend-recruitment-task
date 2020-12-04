513865b9086ff4369f18447d7643da69de55b8f1
ola.krecisz <ola.krecisz@tylko.com>
2020-12-04 11:44:35 +0100

    add first page

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,64 @@
 <template>
-  <div class="welcome-page">
-    <div>
-      <img class="welcome-page__logo"
-           src="~/assets/images/tylko-logo.svg"
-           svg-inline
-      >
-
-      <h1 class="bold-32 md:bold-72 text-white my-40">
-        Shelves that fit every home
-      </h1>
-      <div class="row">
-        <a
-          href="https://docs.google.com/document/d/19DMdkIsiqxsZTsZ5RxQNtPI1XpWCdAabrqdEbm4vl-g/edit"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="welcome-page__button"
-        >
-          Design system
-        </a>
-        <a
-          href="https://www.figma.com/file/4I9prCmnLdCNHaClurysFg/Zadanie-rekrutacyjne-Tylko?node-id=0%3A1"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="welcome-page__button welcome-page__button--secondary"
-        >
-          Figma
-        </a>
-      </div>
-    </div>
-  </div>
+    <section class="welcome-page">
+        <div class="container-cstm-fluid py-48 md:py-96">
+            <img class="welcome-page__logo"
+                 src="~/assets/images/tylko-logo.svg"
+                 svg-inline
+            >
+            <div class="row">
+                <h1 class="col-xs-8 col-md-6 col-lg-5 col-xl-4 bold-32 md:bold-72 text-white mt-8 mb-16 md:my-40">
+                    Shelves that fit every home
+                </h1>
+            </div>
+            <div class="flex">
+                <a href="https://docs.google.com/document/d/19DMdkIsiqxsZTsZ5RxQNtPI1XpWCdAabrqdEbm4vl-g/edit"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                   class="button"
+                >
+                    Design system
+                </a>
+                <a href="https://www.figma.com/file/4I9prCmnLdCNHaClurysFg/Zadanie-rekrutacyjne-Tylko?node-id=0%3A1"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                   class="button button--light ml-24"
+                >
+                    Figma
+                </a>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-export default {}
+    export default {}
 </script>
 
 <style lang="scss" scoped>
-$transition: 0.2s ease-in-out;
+    .welcome-page {
+        background: url("~assets/images/welcome-hero_s.jpg") right top no-repeat;
+        background-size: cover;
+        height: 100vh;
 
-.welcome-page {
-  background: url("~assets/images/welcome-hero_s.jpg") center center no-repeat;
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
+        @include tablet-min {
+            background-image: url("~assets/images/welcome-hero_l.jpg");
+            background-position: center center;
+            justify-content: center;
+        }
 
-  @include tablet-min {
-    background-image: url("~assets/images/welcome-hero_l.jpg");
-  }
+        @include desktop-xl2 {
+            background-image: url("~assets/images/welcome-hero_xl.jpg");
+        }
 
-  @include desktop-xl2 {
-    background-image: url("~assets/images/welcome-hero_xl.jpg");
-  }
+        &__logo {
+            height: 29px;
+            width: 81px;
 
-  &__logo {
-    height: 60px;
-  }
-
-  &__button {
-    border-radius: 50px;
-    cursor: pointer;
-    display: inline-flex;
-    align-items: center;
-    transition: background-color $transition, border-color $transition;
-    text-decoration: none;
-
-    &:hover, &:focus, &:active {
-        text-decoration: none;
-        background-color: $ds-orange-900;
-        border: 1px solid $ds-orange-900;
-        outline: none;
+            @include tablet-min {
+                height: 58px;
+                width: 162px;
+            }
+        }
     }
-  }
-}
 
 </style>
